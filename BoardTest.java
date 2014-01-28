c7223d4557d8a1da6af8573ced3fcb5040d0b10d
KaiUR <kairathjen@yahoo.com>
2014-01-28 13:31:36 +0000

    Initial Commit

---
+++ BoardTest.java
@@ -0,0 +1,28 @@
+package Assignment1Backgammon;
+
+/**
+ * A tester class used to test the Board class
+ * 
+ * @author Laurence Quinn, Cairan O'Niell, Kai-Uwe Rathjen
+ * @version 0.03, 26 JAN 2014
+ * @see Assignment1Backgammon
+ */
+public class BoardTest
+{
+
+	/**
+	 * The Main method of this tester class
+	 * 
+	 * @param args
+	 *            Not used in this program
+	 */
+	public static void main(String[] args)
+	{
+
+		Board myBoard = new Board();
+		myBoard.printBoard();
+
+		System.exit(0);
+	}
+
+}
