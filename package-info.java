8cd75f6a6e78e869e54f3f711407e9f5a8de807b
KaiUR <kairathjen@yahoo.com>
2014-01-31 20:47:00 +0000

    Create package-info.java

---
+++ package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains all of the files required  for Assignment 1
+ */
+/**
+ * @author Laurence Quinn, Ciarán O'Niell, Kai-Uwe Rathjen
+ *
+ */
+package assignment1Backgammon;
