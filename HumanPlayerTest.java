45ece7bf02fadddae7652f10d69328cb2c9c25c5
lorrieq <lorrieq@hotmail.com>
2014-02-16 16:32:51 +0000

    Create HumanPlayerTest.java

---
+++ HumanPlayerTest.java
@@ -0,0 +1,13 @@
+package assignment1Backgammon;
+
+public class HumanPlayerTest {
+	
+	public static void main(String[] args) {
+		
+		Board myBoard = new Board();
+		HumanPlayer player1 = new HumanPlayer(0);
+		player1.makeMove(myBoard);
+		
+	}
+	
+}
