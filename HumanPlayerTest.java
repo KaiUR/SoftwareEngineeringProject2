4e42b85daf0baa7e31446ff164ccc8616865badf
KaiUR <kairathjen@yahoo.com>
2014-02-17 13:36:55 +0000

    Update HumanPlayerTest.java

--- HumanPlayerTest.java
+++ HumanPlayerTest.java
@@ -5,9 +5,21 @@ public class HumanPlayerTest {
 	public static void main(String[] args) {
 		
 		Board myBoard = new Board();
-		HumanPlayer player1 = new HumanPlayer(0);
-		player1.makeMove(myBoard);
-		
+		HumanPlayer player1 = new HumanPlayer(0, myBoard);
+		HumanPlayer player2 = new HumanPlayer(1, myBoard);
+		while (true)
+		{
+			if (player1.makeMove() == -2)
+			{
+				break;
+			}
+			if (player2.makeMove() == -2)
+			{
+				break;
+			}
+		}
+		player1.closeScanner();
+		player2.closeScanner();
 	}
 	
 }
