136f10059504c7e431b7eeac7a9dbca26e5cb562
KaiUR <kairathjen@yahoo.com>
2014-03-22 20:12:51 +0000

    Update BgTwoPlayer.java

--- BgTwoPlayer.java
+++ BgTwoPlayer.java
@@ -1,6 +1,14 @@
 package assignment3Backgammon;
 
-public class BgTwoPlayer {
+/**
+ * @author Laurence Quinn 12473478, Ciarán O'Niell 12432672, Kai-Uwe Rathjen
+ *         12343046
+ * @version 1.01, 22 MAR 2014
+ * @see assignment3Backgammon;
+ * 
+ */
+public class BgTwoPlayer 
+{
 	
 	/*
 	 * Create initial board and two player objects
