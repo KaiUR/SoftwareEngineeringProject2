2ebd1b10200bc9b89ada50c8b877a71adca796b9
KaiUR <kairathjen@yahoo.com>
2014-04-21 20:12:24 +0100

    Update AiPlayer.java

--- Assignment5/AiPlayer.java
+++ Assignment5/AiPlayer.java
@@ -26,10 +26,96 @@ public class AiPlayer {
 	}
 	
 	
-	private int findBestBoard (ArrayList<Board> allBoardsList) {
+	/**
+	 * This method will select the best move possible
+	 * 
+	 * @param allBoardsList
+	 *            The list of all plays
+	 * @return The index of the best move
+	 */
+	private int findBestBoard(ArrayList<Board> allBoardsList)
+	{
 		int bestBoard = 0;
+
+		if (FirstMove == true)
+		{
+			FirstMove = false;
+
+			if (checkGoingFirst())
+			{
+				bestBoard = goingFirst(allBoardsList);
+			}
+			else
+			{
+				bestBoard = goingSecond(allBoardsList);
+			}
+		}
+		else
+		{
+			bestBoard = normalMove(allBoardsList);
+		}
+
 		return bestBoard;
 	}
+
+	/**
+	 * UNIMPLEMENTED
+	 * 
+	 * This method is used to check if the player is opening first or if the
+	 * player is going second
+	 * 
+	 * Suggestion: Check if gameBoard.checkers[playerId][0 to 26] matches the
+	 * starting board, if yes that player is going first.
+	 * 
+	 * @return True if the player is going first
+	 */
+	private boolean checkGoingFirst()
+	{
+		return true;
+	}
+
+	/**
+	 * UNIMPLEMENTED
+	 * 
+	 * This method is used to make the first move if the player is going first
+	 * 
+	 * @param allBoardsList
+	 *            The list of all plays
+	 * @return The index of the best move
+	 */
+	private int goingFirst(ArrayList<Board> allBoardsList)
+	{
+		return 0;
+	}
+
+	/**
+	 * UNIMPLEMENTED
+	 * 
+	 * This method is used to make the first move if the player is going second
+	 * 
+	 * @param allBoardsList
+	 *            The list of all plays
+	 * @return The index of the best move
+	 */
+	private int goingSecond(ArrayList<Board> allBoardsList)
+	{
+		return 0;
+	}
+
+	/**
+	 * UNIMPLEMENTED
+	 * 
+	 * This method applys the strategy after the players first move
+	 * 
+	 * @param allBoardsList
+	 *            The list of all plays
+	 * @return The index of the best move
+	 */
+	private int normalMove(ArrayList<Board> allBoardsList)
+	{
+		return 0;
+	}
+
 	
 	
 	public Play getPlay () {
